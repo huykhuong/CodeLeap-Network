fa7f116560bc7c915547ea35ac2f35aabaa2bd8f
Huy Khuong <scottkhuong@gmail.com>
2022-09-01 11:12:17 +0700

    [9/1/2022] - Debug newer CSS Module issues when deployed (attempt 2)

--- src/components/MainScreen/MainSection.js
+++ src/components/MainScreen/MainSection.js
@@ -1,6 +1,5 @@
 import React from "react";
 import styles from "./MainSection.module.css";
-
 const MainSection = ({ children }) => {
   return <main className={styles.main_section__container}>{children}</main>;
 };
